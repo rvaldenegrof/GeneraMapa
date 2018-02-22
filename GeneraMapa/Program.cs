4ab50ac511accfc267149bf8fcbd71bef836394a
rvaldenegrof <r.valdenegrof@gmail.com>
2018-02-22 17:23:11 -0300

    Control de existencia de archivo (Try-Catch)

--- GeneraMapa/Program.cs
+++ GeneraMapa/Program.cs
@@ -59,46 +59,55 @@ namespace GeneraMapa
         public static void Procesa_Excel(String Archivo_Excel, String Archivo_Temp, int Excel_Columna_Desde, int Excel_Fila_Desde, int Considera_Ultima_Columna , String strNombreHoja)
         {
 
-            Excel.Application excelApp = new Excel.Application(); // Variable que corresponde con la app Excel.
-            Excel.Workbook Workbooks = excelApp.Workbooks.Open(Archivo_Excel); // Abre el archivo Excel.
-            Excel.Worksheet Hoja; // Variable que contendrá la hoja de Excel a procesar.
-            Excel.Range data; // Variable que contendrá la data de la hoja de Excela que se procesará.
-
-            Hoja = (Excel.Worksheet)Workbooks.Worksheets[strNombreHoja]; // Accede a la hoja que contiene los datos.
-            data = Hoja.UsedRange; // Variable data queda con los datos que existen en la hoja.
-
-
-            string strdato; // Variable que tomara dato de la celda que se este procesando.
-            int rCnt = 0; // Variable asociada a fila (Row) que contiene información en la hoja de Excel.
-            int cCnt = 0; // Variable asociada a la columna (Column) que contiene información en la hoja de Excel.
-
-            String strLinea = ""; // String donde se irá almacenando la información de cada fila separa por un tabulador.
-            String strTodaLaInfo = ""; //String que contendrá toda la información recuperada del excel.
-
-            for (rCnt = Excel_Fila_Desde; rCnt <= data.Rows.Count; rCnt++) // Ciclo for para recorrer por filas.
+            try
             {
-                strLinea = string.Empty; // Cada vez que comience una nueva columna, se limpia este String, para que almacene solo lo de la linea a procesar.
-                for (cCnt = Excel_Columna_Desde; cCnt <= data.Columns.Count - Considera_Ultima_Columna; cCnt++) // ciclo for para recorrer cada columna de la fila que se está procesando.
+
+                Excel.Application excelApp = new Excel.Application(); // Variable que corresponde con la app Excel.
+                Excel.Workbook Workbooks = excelApp.Workbooks.Open(Archivo_Excel); // Abre el archivo Excel.
+                Excel.Worksheet Hoja; // Variable que contendrá la hoja de Excel a procesar.
+                Excel.Range data; // Variable que contendrá la data de la hoja de Excela que se procesará.
+
+                Hoja = (Excel.Worksheet)Workbooks.Worksheets[strNombreHoja]; // Accede a la hoja que contiene los datos.
+                data = Hoja.UsedRange; // Variable data queda con los datos que existen en la hoja.
+
+
+                string strdato; // Variable que tomara dato de la celda que se este procesando.
+                int rCnt = 0; // Variable asociada a fila (Row) que contiene información en la hoja de Excel.
+                int cCnt = 0; // Variable asociada a la columna (Column) que contiene información en la hoja de Excel.
+
+                String strLinea = ""; // String donde se irá almacenando la información de cada fila separa por un tabulador.
+                String strTodaLaInfo = ""; //String que contendrá toda la información recuperada del excel.
+
+                for (rCnt = Excel_Fila_Desde; rCnt <= data.Rows.Count; rCnt++) // Ciclo for para recorrer por filas.
                 {
-                    // Chiche para dar la impresión de que hace algo impresionante.
-                    Console.Write(".");
-
-                    strdato = (string)(data.Cells[rCnt, cCnt] as Excel.Range).Text; // obtiene dato de la celda.
-
-                    if (String.IsNullOrEmpty(strLinea) ) // si está vacío, parte con el dato.
+                    strLinea = string.Empty; // Cada vez que comience una nueva columna, se limpia este String, para que almacene solo lo de la linea a procesar.
+                    for (cCnt = Excel_Columna_Desde; cCnt <= data.Columns.Count - Considera_Ultima_Columna; cCnt++) // ciclo for para recorrer cada columna de la fila que se está procesando.
                     {
-                        strLinea += strdato;
+                        // Chiche para dar la impresión de que hace algo impresionante.
+                        Console.Write(".");
+
+                        strdato = (string)(data.Cells[rCnt, cCnt] as Excel.Range).Text; // obtiene dato de la celda.
+
+                        if (String.IsNullOrEmpty(strLinea)) // si está vacío, parte con el dato.
+                        {
+                            strLinea += strdato;
+                        }
+                        else
+                        {
+                            strLinea += "\t" + strdato; // Si ya tiene data, parte con un tabulador y le agrega el dato.
+                        }
                     }
-                    else
-                    {
-                        strLinea += "\t" + strdato; // Si ya tiene data, parte con un tabulador y le agrega el dato.
-                    }
+                    strTodaLaInfo += strLinea + "\n"; // Cuando termina de procesar todas las columnas, le agrega lo recolectado y le da un "Enter".
                 }
-                strTodaLaInfo += strLinea + "\n"; // Cuando termina de procesar todas las columnas, le agrega lo recolectado y le da un "Enter".
+
+                File.WriteAllText(Archivo_Temp, strTodaLaInfo); // una vez finalizado de recorrer la hoja con datos del excel, graba el resultado en un archivo temporal.
+
             }
+            catch (Exception e) {
 
-            File.WriteAllText(Archivo_Temp, strTodaLaInfo); // una vez finalizado de recorrer la hoja con datos del excel, graba el resultado en un archivo temporal.
-
+                Console.WriteLine("No se encuentra archivo de carga ", e);
+                Console.ReadLine();
+            }
         }
 
         public static void Procesa_Archivo_Temporal(int Data_Desde, int Data_Hasta, String Archivo_Temp)
